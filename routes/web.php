7e49099399d75336833f6f9d2e03d683d7d28d50
ivanProgramador <ivan-guita@hotmail.com>
2025-07-25 21:53:43 -0300

    sobre conexao laravel e base de dados

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,44 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\DB;
 
-Route::get('/', function () {
-    return view('home');
+
+//rota para testar a conexão com mysql 
+
+Route::get('/mysql',function(){
+     //Ele vai tentar se conectar 
+
+     try{
+
+         //sele ele conseguir ele vai mostrar o nome da base de dados na tela 
+         DB::connection()->getPdo();
+         echo"Conexão com a base de dados: ".DB::connection()->getDatabaseName();
+       
+     }catch(\Exception $e){
+
+        // se ele não conseguir ele vai mostrar uma mensagem de erro 
+
+         die("Não foi possivel se conectar com a base de dados : ".$e->getMessage());
+     }
+
+});
+
+Route::get('/sqllite',function(){
+
+    //Ele vai tentar se conectar 
+
+     try{
+
+         //sele ele conseguir ele vai mostrar o nome da base de dados na tela 
+         DB::connection()->getPdo();
+         echo"Conexão com a base de dados: ".DB::connection()->getDatabaseName();
+       
+     }catch(\Exception $e){
+
+        // se ele não conseguir ele vai mostrar uma mensagem de erro 
+
+         die("Não foi possivel se conectar com a base de dados : ".$e->getMessage());
+     }
+
 });
