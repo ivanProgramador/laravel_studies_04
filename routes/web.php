bc0a022878420316dff47826059250110d5a2515
Ivan Rodrigues <ivan-guita@hotmail.com>
2025-07-27 13:14:46 -0300

    tipos de conexao , compreendidos e implementados

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Dba\Connection;
+use Illuminate\Support\Facades\Config;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\DB;
 
@@ -85,6 +86,43 @@ Route::get('/mysql_test_two_databases',function(){
          die('Erro:'.$e->getMessage());
     }
 });
+
+Route::get('/dynamic_connection',function(){
+
+    try{
+
+        Config::set('database.connections.mysql',[
+    'driver' => 'mysql',
+    'url' => '',
+    'host' => '127.0.0.1',
+    'port' => '3306',
+    'database' => 'laravel_mysql_app',
+    'username' => 'root',
+    'password' => '',
+    'unix_socket' => env('DB_SOCKET', ''),
+    'charset' => env('DB_CHARSET', 'utf8mb4'),
+    'collation' => env('DB_COLLATION', 'utf8mb4_unicode_ci'),
+     'prefix' => '',
+     'prefix_indexes' => true,
+     'strict' => true,
+     'engine' => null,
+        'options' => extension_loaded('pdo_mysql') ? array_filter([
+         PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
+       ]) : [],
+   ]);
+
+        DB::connection('mysql')->getPdo();
+        echo'ligação ok';
+        
+
+
+    }catch(\Exception $e){
+
+         die('Erro:'.$e->getMessage());
+    }
+});
+
+
 
 
 
